45d69810a0d491f8e300344eeb45f47f848a9df4
Sahar Soffer <ssoffer@medallia.com>
2021-07-22 17:16:15 +0300

    add asset management to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,32 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  // asset management
+  module: {
+    rules: [
+      // css
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // images
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      // fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      // json
+     {
+       test: /\.json5$/i,
+       type: 'json',
+       parser: {
+         parse: json5.parse,
+       },
+     },
+    ],
+  },  
 };
