783741f08fcead7d4d2d25fad75888e154aa2e70
Sahar Soffer <ssoffer@medallia.com>
2021-07-22 14:09:14 +0300

    remove lodash from example code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import join from 'lodash';
 import foo from './foo';
-console.log(foo);
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = join(['Hello', 'webpack'], ' ');
-  
+    element.innerHTML = foo.foo;
     return element;
 }
   
