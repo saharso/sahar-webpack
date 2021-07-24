fe90c5a9846ace22c96cade2d780ba81e87f4134
Sahar Soffer <ssoffer@medallia.com>
2021-07-24 21:35:17 +0300

    x

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, {Suspense} from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as BrowserRouter, Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import './style.scss';
 
@@ -11,13 +11,15 @@ const Another = React.lazy(() => import('./Another.jsx'));
 const Loader = ()=> <div>loading...</div>
 
 const appRouting = (
-  <BrowserRouter>
-  FOO
+  <>
+  FOOT
+  <Router basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route exact path="/" render={App} />
       <Route path="/another" render={()=><Suspense fallback={<><Loader/></>}><Another/></Suspense>} />
     </Switch>
-  </BrowserRouter>
+  </Router>
+  </>
 );
 
 ReactDOM.render(appRouting, document.getElementById("root"));
