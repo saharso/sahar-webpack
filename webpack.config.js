0d4359a747fd82019da6b31d62809e6def950c9f
Sahar Soffer <ssoffer@medallia.com>
2021-07-24 21:48:10 +0300

    x

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     index: './src/index.tsx',
   },
   // for development env:
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
